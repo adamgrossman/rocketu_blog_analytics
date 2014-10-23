225e94ec5fde8ddc75354e4e35c822f3a3dcd184
Adam <adam.grossman08@me.com>
2014-10-23 15:06:04 -0700

    mix panel 2

--- static/js/mixpanel.js
+++ static/js/mixpanel.js
@@ -5,4 +5,8 @@ $(document).ready(function() {
     $("#featuredBlog").click(function () {
         mixpanel.track("Blog Footer Clicked");
     });
+
+    $(".sideTag").click(function () {
+        mixpanel.track("Blog Footer Clicked");
+    });
 });
